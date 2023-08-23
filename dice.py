bde49706eff95c13027bf1ac1a664e24db74722c
Raquel <raquelgoulart@gmail.com>
2023-08-24 00:03:48 +0100

    update_dice

--- dice.py
+++ dice.py
@@ -1,3 +1,32 @@
-import random # necessário para utilizar o módulo random
+#dice.py
 
-print(random.randint(1,6))
+# importing module
+import random 
+
+#definition of function dice
+def dice():
+    return random.randint(1,6)
+
+#input
+start = input("Do you want to roll the dice? (y/n) ")
+
+while True:
+    
+    if start == "y":
+        print("The number is: ", dice())
+    
+    elif start == "n":
+        print("Ok. Let's play later.")
+        break #Finish the game
+
+    else:
+        print("Thanks for playing.")
+
+    repeat = input("Do you want to roll the dice again? (y/n) ")
+    if repeat != "y":
+        print ("Thanks for playing.")
+        break  # Finish the game
+        
+
+
+
